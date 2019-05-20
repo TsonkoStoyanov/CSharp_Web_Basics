d03fa68c22a98b3757a69053d97808cae8ac1b96
TsonkoStoyanov <32267325+TsonkoStoyanov@users.noreply.github.com>
2019-05-20 18:11:08 +0300

    parallel programming Chronometer

---
+++ 02_Exercise_Web_Server_Asynchronous_Processing/Chronometer/Chronometer/Chronometer/Chronometer.cs
@@ -0,0 +1,67 @@
+﻿using Chronometer.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private long milliseconds;
+        private bool isRunning;
+
+        public Chronometer()
+        {
+            this.Reset();
+        }
+
+
+        public List<string> Laps { get; private set; }
+
+        public string GetTime => $"{milliseconds / 60000:D2}:{milliseconds / 1000:D2}:{milliseconds % 1000:D4}";
+
+        public string Lap()
+        {
+
+            string currentLap = GetTime;
+            this.Laps.Add(currentLap);
+
+            return currentLap;
+        }
+
+        public void Reset()
+        {
+            this.Stop();
+            this.milliseconds = 0;
+            this.Laps = new List<string>();
+            
+        }
+
+        public void Start()
+        {
+            this.isRunning = true;
+
+            Task.Run(() =>
+            {
+                while (this.isRunning)
+                {
+                    Thread.Sleep(1);
+                    this.milliseconds++;
+                }
+            });
+
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+
+
+        public override string ToString()
+        {
+            return GetTime;
+        }
+    }
+}
---
+++ 02_Exercise_Web_Server_Asynchronous_Processing/Chronometer/Chronometer/Chronometer/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using Chronometer.Contracts;
+using System;
+
+namespace Chronometer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            IChronometer chronometer = new Chronometer();
+
+            while ((input = Console.ReadLine()) != "exit")
+            {
+                switch (input)
+                {
+                    case "start":
+                        chronometer.Start();
+                        break;
+
+                    case "stop":
+                        chronometer.Stop();
+                        break;
+
+                    case "reset":
+                        chronometer.Reset();
+                        break;
+
+                    case "lap":
+                        chronometer.Lap();
+                        Console.WriteLine(chronometer);
+                        break;
+
+                    case "time":
+                        Console.WriteLine(chronometer);
+                        break;
+
+                    case "laps":
+                        if (chronometer.Laps.Count < 1)
+                        {
+                            Console.WriteLine($"Laps: no laps");
+                        }
+                        else
+                        {
+                            int count = 0;
+
+                            foreach (var lap in chronometer.Laps)
+                            {
+                                Console.WriteLine($"{count++}.{lap}");
+                            }
+                        }
+
+                        break;
+                }
+
+            }
+        }
+    }
+}
